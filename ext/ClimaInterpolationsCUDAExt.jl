dff432697c50931150da3c1c4f7abe491b3a0874
sriharshakandala <sriharsha.kvs@gmail.com>
2025-02-14 14:04:13 -0800

    Query maximum grid dimension.

--- ext/ClimaInterpolationsCUDAExt.jl
+++ ext/ClimaInterpolationsCUDAExt.jl
@@ -13,6 +13,20 @@ import ClimaInterpolations.Interpolation1D:
     LinearExtrapolation,
     get_stencil
 
+"""
+    get_maxgridsize()
+
+Returns the maximum allowed logical `CUDA` grid dimensions for the current device.
+"""
+function get_maxgridsize()
+    device = CUDA.device()
+    return (
+        CUDA.attribute(device, CUDA.DEVICE_ATTRIBUTE_MAX_GRID_DIM_X),
+        CUDA.attribute(device, CUDA.DEVICE_ATTRIBUTE_MAX_GRID_DIM_Y),
+        CUDA.attribute(device, CUDA.DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z),
+    )
+end
+
 include(joinpath("cuda", "interpolation1d.jl"))
 include(joinpath("cuda", "bilinearinterpolation.jl"))
 end # module
